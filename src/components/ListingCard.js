b3371f558cfa87e39c4f54b2a556a2c716f0980e
Astika Luintel <astild2020@gmail.com>
2022-09-27 10:26:59 -0400

    match the two delivery

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,20 +1,25 @@
-import React from "react";
+import {React, useState}from "react";
 
-function ListingCard() {
+function ListingCard({listing}) {
+  const[favorite, setFavorite] = useState(false)
+
+  function toggleFavorite() {
+    setFavorite(!favorite)
+  }
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image} alt={listing.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {favorite ? (
+          <button className="emoji-button favorite active" onClick={toggleFavorite}>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite" onClick={toggleFavorite}>☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings}) {
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map(listing=>{
+          console.log(listing)
+          return <ListingCard key={listing.id} listing={listing}  />;
+          
+        })}
       </ul>
     </main>
   );
