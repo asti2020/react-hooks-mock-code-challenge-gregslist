0d43cdeeeb544de08ae9b63a411a865d807cd1ba
Astika Luintel <astild2020@gmail.com>
2022-09-27 10:49:44 -0400

    i can post it now

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import {React, useEffect, useState} from "react";
+import  FormListing  from "./FormListing";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
-
-
 function App() {
   const[listings, setListings] = useState([])
+  // const [card, setCard] = useState([])
 
     useEffect(() =>{
       fetch(" http://localhost:6001/listings")
@@ -15,11 +15,15 @@ function App() {
       });
 
     },[])
-    
+
+    const newListing =(card) =>{
+      setListings([...listings, card])
+    }
     return (
       <div className="app">
         <Header />
         <ListingsContainer  listings={listings}/>
+        <FormListing  newListing={newListing}/>
       </div>
     );
 }
---
+++ src/components/FormListing.js
@@ -0,0 +1,45 @@
+import {React, useState} from 'react'
+
+function FormListing ({newListing}) {
+    const[description, setDescription] = useState("");
+    const[image, setImage] = useState("");
+    const[location, setLocation] = useState("");
+
+    const handleDescription =(e) =>{
+        setDescription(e.target.value)
+    }
+
+    const handleImage =(e) =>{
+        setImage(e.target.value)
+    }
+    const handleLocation =(e) =>{
+        setLocation(e.target.value)
+    }
+    const handleSubmit =(e) =>{
+        e.preventDefault()
+       fetch("http://localhost:6001/listings" ,{
+        method: "POST",
+            headers: {
+                "content-type": "application/json",
+            }, body: JSON.stringify({
+                description: description,
+                image: image,
+                location: location
+            }),
+       })
+       .then((res) => res.json())
+       .then((list) => {
+        newListing(list)
+       })
+    }
+  return (
+    <form onSubmit={handleSubmit}>
+        <input onChange={handleDescription} type="text" placeholder="description" value={description} />
+        <input onChange={handleImage} type="src" placeholder="image" value={image} />
+        <input onChange={handleLocation} type="lcation" placeholder="Location" value={location}/>
+        <button type="submit">post</button>
+    </form>
+  )
+}
+
+export default FormListing
